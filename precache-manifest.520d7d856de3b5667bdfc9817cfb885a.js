7caedfdecf7483ad338285a6fd4d0553f5c9a5a2
Colin Brayton <gringolalia@gmail.com>
2019-10-26 16:28:52 -0300

    deploy

---
+++ precache-manifest.520d7d856de3b5667bdfc9817cfb885a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "396fd44119feddaa32da",
+    "url": "/complicated-beauty/js/about.83f3eecc.js"
+  },
+  {
+    "revision": "a288e0f1de9447ff60f0",
+    "url": "/complicated-beauty/js/app.a96d3afa.js"
+  },
+  {
+    "revision": "993eb968fc528793625d",
+    "url": "/complicated-beauty/css/chunk-vendors.e5760afd.css"
+  },
+  {
+    "revision": "993eb968fc528793625d",
+    "url": "/complicated-beauty/js/chunk-vendors.183f3ccb.js"
+  },
+  {
+    "revision": "875141d94aca807fbf74f7f53fa98a26",
+    "url": "/complicated-beauty/fonts/materialdesignicons-webfont.875141d9.woff"
+  },
+  {
+    "revision": "815a2673d08221fb4968f19e59a14f2e",
+    "url": "/complicated-beauty/fonts/materialdesignicons-webfont.815a2673.woff2"
+  },
+  {
+    "revision": "f558fb5cc12a31989a7e4048b4fba71a",
+    "url": "/complicated-beauty/fonts/materialdesignicons-webfont.f558fb5c.ttf"
+  },
+  {
+    "revision": "666e135393ac4ac62ae551b9fb0985ae",
+    "url": "/complicated-beauty/fonts/materialdesignicons-webfont.666e1353.eot"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/complicated-beauty/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/complicated-beauty/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/complicated-beauty/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/complicated-beauty/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/complicated-beauty/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/complicated-beauty/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/complicated-beauty/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/complicated-beauty/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/complicated-beauty/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/complicated-beauty/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/complicated-beauty/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/complicated-beauty/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/complicated-beauty/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/complicated-beauty/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/complicated-beauty/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/complicated-beauty/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/complicated-beauty/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/complicated-beauty/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/complicated-beauty/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/complicated-beauty/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/complicated-beauty/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/complicated-beauty/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/complicated-beauty/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/complicated-beauty/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/complicated-beauty/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "3b1a63340c93a70c0594c9fff28829a2",
+    "url": "/complicated-beauty/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/complicated-beauty/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/complicated-beauty/precache-manifest.520d7d856de3b5667bdfc9817cfb885a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "complicated-beauty"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
